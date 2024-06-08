c58f6eea2dd0edb021c235fb9f371954c1f2b748
darshansidev <darshansidev@gmail.com>
2024-06-08 11:33:39 +0530

    Add Todo Project With using Next14

---
+++ utils/prisma.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+
+
+// import { PrismaClient } from '@prisma/client'
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
